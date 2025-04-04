41c4f9cbcf062a3db518f6137e2474fa3523431a
Akanni-codes <akanni029@gmail.com>
2025-04-04 19:27:20 -0300

    Implemetanção camada de dominio

---
+++ src/main/java/Me/dio/Avana_bootcamp_2025/domain/model/News.java
@@ -0,0 +1,12 @@
+package Me.dio.Avana_bootcamp_2025.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
+
---
+++ src/main/java/Me/dio/Avana_bootcamp_2025/domain/model/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package Me.dio.Avana_bootcamp_2025.domain.model.Repository;
+
+import Me.dio.Avana_bootcamp_2025.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
