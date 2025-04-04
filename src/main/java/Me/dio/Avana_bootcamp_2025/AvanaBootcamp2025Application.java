87c377eb935ff98c36c28f283ecc6f5eb62443d5
Akanni-codes <akanni029@gmail.com>
2025-04-04 15:13:08 -0300

    Initial commit

---
+++ src/main/java/Me/dio/Avana_bootcamp_2025/AvanaBootcamp2025Application.java
@@ -0,0 +1,13 @@
+package Me.dio.Avana_bootcamp_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanaBootcamp2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanaBootcamp2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/Me/dio/Avana_bootcamp_2025/AvanaBootcamp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package Me.dio.Avana_bootcamp_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanaBootcamp2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
