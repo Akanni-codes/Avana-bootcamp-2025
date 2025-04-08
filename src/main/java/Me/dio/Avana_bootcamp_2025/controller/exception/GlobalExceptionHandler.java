3b4bf348b282eea5e77b07bd61104d8e85e14dd1
Akanni-codes <akanni029@gmail.com>
2025-04-08 19:01:15 -0300

    testando versionamento do spring

---
+++ src/main/java/Me/dio/Avana_bootcamp_2025/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+//package Me.dio.Avana_bootcamp_2025.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+
+//@RestControllerAdvice
+//public class GlobalExceptionHandler {
+
+   // private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+   // @ExceptionHandler(IllegalArgumentException.class)
+   // public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+       // return ResponseEntity
+       //         .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        //        .body(ex.getMessage());
+  //  }
+
+   // @ExceptionHandler(NoSuchElementException.class)
+  //  public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+       // return ResponseEntity
+            //    .status(HttpStatus.NOT_FOUND)
+             //   .body("Resource ID not found");
+  //  }
+
+   // @ExceptionHandler(Throwable.class)
+   // public ResponseEntity<String> handleUnexpectedException(Throwable ex) {
+      //  String message = "Unexpected server error";
+     //   logger.error(message, ex);
+     //   return ResponseEntity
+        //        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+       //         .body(message);
+  //  }
+//}
